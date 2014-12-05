c57ec92116ddb6b883806e7747578b1fe3862148
mattdesl <dave.des@gmail.com>
2014-12-04 23:21:20 -0500

    cleaning up source

--- common.js
+++
@@ -1,9 +0,0 @@
-var eases = require('eases')
-var xtend = require('xtend')
-var base = require('./index')
-
-module.exports = function Tweenr(opt) {
-    return base(xtend({ eases: eases }, opt))
-}
-
-var tweenr = require('tweenr/core')()
